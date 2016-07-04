7ee6f62b20c9c5b08b5e9b10c1611f8649f20f56
Matt Yost <matt.yost2@gmail.com>
2016-07-04 00:01:57 -0500

    loads initial info via JS
    Made a lot of changes to make things simpler

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-'use strict'
+$(document).ready(function(){
+
+  // Get UL info
+  var studentList = $('.student-list li');
+
+  // Append search bar to header
+  var searchHeader = "<h2>Students</h2><div class='student-search'><input placeholder='Search for students...'><button>Search</button></div>";
+  $(".page-header").html(searchHeader);
+
+  // Calculate number of pages needed
+  var numberOfPages = Math.ceil(studentList.length / 10);
+  
+  var pagination = function(){
+    paginationHTML = "<ul>";
+    for(var i = 0; i < numberOfPages; i++){
+      paginationHTML+= "<li><a href='#'>" + (i+1) + "</li>";
+    };
+    paginationHTML += "</ul>";
+    $(".pagination").html(paginationHTML);
+  };
+  pagination();
+
+  // Show first 10 students
+  studentList.hide();
+  studentList.slice(0,10).show();
+
+  //Toggle active class on buttons when clicked
+  var page = $(".pagination li a");
+  $(page).on("click", function(){
+      page.removeClass("active");
+      $(this).toggleClass("active");
+  });
+
+  // Seperate students into x amount of pages
+  studentList.each(function(index){
+    //console.log($(this).html());
+  });
+
+
+  // Search function
+
+});
