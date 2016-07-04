74bc17082fc776a6c48e9f51eee57510419d73c4
Matt Yost <matt.yost2@gmail.com>
2016-07-04 16:30:35 -0500

    10(or less) students per page
    10 students per page until less than 10 students are added to a page

--- main.js
+++ main.js
@@ -1,42 +1,45 @@
-$(document).ready(function(){
+// Get UL info
+var studentList = $('.student-list li');
 
-  // Get UL info
-  var studentList = $('.student-list li');
+// Append search bar to header
+var searchHeader = "<h2>Students</h2><div class='student-search'><input placeholder='Search for students...'><button>Search</button></div>";
+$(".page-header").html(searchHeader);
 
-  // Append search bar to header
-  var searchHeader = "<h2>Students</h2><div class='student-search'><input placeholder='Search for students...'><button>Search</button></div>";
-  $(".page-header").html(searchHeader);
+// Calculate number of pages needed
+var numberOfPages = Math.ceil(studentList.length / 10);
 
-  // Calculate number of pages needed
-  var numberOfPages = Math.ceil(studentList.length / 10);
-  
-  var pagination = function(){
-    paginationHTML = "<ul>";
-    for(var i = 0; i < numberOfPages; i++){
-      paginationHTML+= "<li><a href='#'>" + (i+1) + "</li>";
-    };
-    paginationHTML += "</ul>";
-    $(".pagination").html(paginationHTML);
+//append 10 students per page until no more students able to append
+var studentsArray = [];
+while(studentList.length > 0){
+  studentsArray.push(studentList.splice(0,10));
+}
+
+// Load Pagination HTML to the page
+var pagination = function(){
+  // Create pagination HTML
+  var paginationHTML = "<ul>";
+  for(var i = 0; i < numberOfPages; i++){
+    paginationHTML+= "<li><a href='#'>" + (i+1) + "</li>";
   };
-  pagination();
+  paginationHTML += "</ul>";
+  $(".pagination").html(paginationHTML);
+  $(".pagination li a").first().toggleClass("active");
+  $(".student-list").html(studentsArray[0]);
+};
+pagination();
 
-  // Show first 10 students
-  studentList.hide();
-  studentList.slice(0,10).show();
-
-  //Toggle active class on buttons when clicked
-  var page = $(".pagination li a");
-  $(page).on("click", function(){
-      page.removeClass("active");
-      $(this).toggleClass("active");
-  });
-
-  // Seperate students into x amount of pages
-  studentList.each(function(index){
-    //console.log($(this).html());
-  });
-
-
-  // Search function
-
+//Toggle active class on buttons when clicked
+var page = $(".pagination li a");
+$(page).on("click", function(){
+  page.removeClass("active");
+  $(this).toggleClass("active");
+  var activePageIndex = $(page).index(this);
+  $(".student-list").html(studentsArray[activePageIndex]);
 });
+
+// Seperate students into x amount of pages
+studentList.each(function(index){
+//console.log($(this).html());
+});
+
+// Search function
